05050542905eff531ca277cdb4203acd2ab79206
kannan.ramasamy <kannanr60@gmail.com>
2025-07-15 23:53:41 +0530

    Two Sum using Classic Array

--- src/array/TwoSum.kt
+++ src/array/TwoSum.kt
@@ -1,2 +1,23 @@
 package array
 
+fun main() {
+    val numbers = intArrayOf(2, 7, 11, 15)
+    val target = 9
+    val result = twoSumNumbers(numbers, target)
+    println("Result is : ${result.joinToString()}")
+}
+
+fun twoSumNumbers(numbers: IntArray, target: Int): IntArray {
+    val numbersMap = HashMap<Int, Int>()
+    var diff = 0
+
+    for (i in numbers.indices) {
+        diff = target - numbers[i]
+        if (numbersMap.containsKey(diff)) {
+            return intArrayOf(numbersMap[diff]!!, i)
+        }
+        numbersMap[numbers[i]] = i
+    }
+
+    return intArrayOf()
+}
